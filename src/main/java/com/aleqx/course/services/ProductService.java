b702f387d21ef64d0c5901b1696ca25f1e310675
Alex Moscato <alexmoscato9044@egmail.com>
2022-05-26 12:04:52 -0400

    Product

---
+++ src/main/java/com/aleqx/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.aleqx.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aleqx.course.entities.Product;
+import com.aleqx.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = productRepository.findById(id);
+		return obj.get();
+	}
+	
+}
