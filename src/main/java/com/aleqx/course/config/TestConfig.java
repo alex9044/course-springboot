1306757f4dc9320ba397eb834abae3aad9c4d2db
Alex Moscato <alexmoscato9044@egmail.com>
2022-06-02 11:49:22 -0400

    Payment, one-to-one association

--- src/main/java/com/aleqx/course/config/TestConfig.java
+++ src/main/java/com/aleqx/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.aleqx.course.entities.Category;
 import com.aleqx.course.entities.Order;
 import com.aleqx.course.entities.OrderItem;
+import com.aleqx.course.entities.Payment;
 import com.aleqx.course.entities.Product;
 import com.aleqx.course.entities.User;
 import com.aleqx.course.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 
