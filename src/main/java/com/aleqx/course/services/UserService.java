b9c609fcf232b0328ed32c2c32b699cb122b487a
Alex Moscato <alexmoscato9044@egmail.com>
2022-06-20 23:13:58 -0400

    User update

--- src/main/java/com/aleqx/course/services/UserService.java
+++ src/main/java/com/aleqx/course/services/UserService.java
@@ -11,26 +11,39 @@ import com.aleqx.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
-	
-	//Operacao para inserir no banco de dados
+
+	// Operacao para inserir no banco de dados
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
-	//Operacao para deletar do banco de dados
+
+	// Operacao para deletar do banco de dados
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+
+	// Operacao para atualizar do banco de dados
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity,obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj){
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
